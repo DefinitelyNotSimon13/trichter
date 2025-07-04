274eaa64e2ab15c3e5d8d39edeead91a5ef0d57e
Simon <simon21.blum@gmail.com>
2025-07-04 17:46:00 +0200

    DevOps stuff

--- leaderboard/src/hooks.server.ts
+++ leaderboard/src/hooks.server.ts
@@ -2,19 +2,25 @@ import { auth } from '$lib/auth';
 import { httpRequestCounter } from '$lib/server/metrics';
 import type { Handle } from '@sveltejs/kit';
 import { svelteKitHandler } from 'better-auth/svelte-kit';
+import * as Sentry from '@sentry/sveltekit';
+import { sequence } from '@sveltejs/kit/hooks';
+import { env } from '$env/dynamic/private';
 
+Sentry.init({
+	dsn: env.SENTRY_DSN,
+	tracesSampleRate: 1
+})
 
+export const handleError = Sentry.handleErrorWithSentry();
 
-export const handle: Handle = async ({ event, resolve }) => {
-	const metricsResolve = async (e: typeof event) => {
-		const res = await resolve(e);
-		httpRequestCounter.labels(e.request.method, e.url.pathname, res.status.toString()).inc();
+export const handle: Handle = sequence(
+	({ event, resolve }) => {
+		return svelteKitHandler({ event, resolve, auth })
+	},
+	Sentry.sentryHandle(),
+	async ({ event, resolve }) => {
+		const res = await resolve(event);
+		httpRequestCounter.labels(event.request.method, event.url.pathname, res.status.toString()).inc();
 		return res;
-	};
-
-	return svelteKitHandler({
-		event,
-		resolve: metricsResolve,
-		auth
-	});
-};
+	}
+);
